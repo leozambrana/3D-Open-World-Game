72072ea022c7faf1c068e4037a6e94d575b89ff6
leonardozambrana1@gmail.com <leonardozambrana1@gmail.com>
2025-05-29 14:37:51 -0300

    add enemy animation and prefab

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -5,6 +5,7 @@ namespace Enemy
 {
     public class EnemyFollow : MonoBehaviour
     {
+        public Animator animator; 
         public Transform player;
         private NavMeshAgent _agent;
         private EnemyAttack _enemyAttack; 
@@ -31,16 +32,19 @@ namespace Enemy
             {
                 _agent.isStopped = false;
                 _agent.SetDestination(player.position);
+                animator.SetFloat("Speed", _agent.velocity.magnitude);
+                animator.ResetTrigger("Attack");
             }
             else
             {
                 _agent.isStopped = true;
-
+                animator.SetFloat("Speed", 0);
                 // Faz o inimigo olhar para o player (opcional, melhora a sensação de ataque)
                 Vector3 lookDirection = (player.position - transform.position).normalized;
                 lookDirection.y = 0;
                 if (lookDirection != Vector3.zero)
                     transform.rotation = Quaternion.LookRotation(lookDirection);
+                animator.SetTrigger("Attack");
             }
         }
     }
